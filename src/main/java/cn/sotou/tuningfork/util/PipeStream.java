fde271731757bed41395a439d6029cef752ca374
shigong <gongshw1992@gmail.com>
2014-03-12 16:53:58 +0800

    prepare to use a packaged Stream

---
+++ src/main/java/cn/sotou/tuningfork/util/PipeStream.java
@@ -0,0 +1,14 @@
+package cn.sotou.tuningfork.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by shigong on 14-3-12.
+ */
+public class PipeStream extends InputStream {
+	@Override
+	public int read() throws IOException {
+		return 0;
+	}
+}
--- src/main/java/cn/sotou/tuningfork/util/PipeUtil.java
+++ src/main/java/cn/sotou/tuningfork/util/PipeUtil.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import cn.sotou.tuningfork.exception.PipeUtilException;
 
 public interface PipeUtil {
+
+	//TODO package InputStream to PipeStream for more feature
 	InputStream[] process(InputStream inputStream, String... args)
 			throws PipeUtilException;
 }
